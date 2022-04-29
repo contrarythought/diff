1dbf5f017c895d81e6fef6dfff10eaa0af38545e
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-04-29 01:52:43 -0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"diff/algo"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("%s <file1.txt> <file2.txt>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	file1, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	file2, err := os.Open(os.Args[2])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	algo.Longest_common_sub(file1, file2)
+
+	file2.Close()
+	file1.Close()
+}
